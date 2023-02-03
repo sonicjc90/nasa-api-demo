1dedc310358623ff69eb33045ffdc39c72f6e980
Stephanie Burgos <burgoss@google.com>
2023-02-03 17:10:20 -0500

    Updating api request

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import requests
 from datetime import date
 
 app = Flask(__name__)
+api_key = 'v5c19WhzIBKZKwuTc0LHo7fX8TSl81hddkCGyXNz'
 
 @app.route('/')
 def index():
     today = str(date.today())
-    response = requests.get("https://api.nasa.gov/planetary/apod?api_key=v5c19WhzIBKZKwuTc0LHo7fX8TSl81hddkCGyXNz&date="+today)
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={today}')
     data = response.json()
-
     return render_template('index.html' , data = data, date = today)
 
 if __name__== '__main__':
